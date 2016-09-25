0771f2640a763814c69a3ef6373b9d113517ee0d
Tymofii Pavliuk <pavliuk@outlook.com>
2016-09-25 10:18:51 +0300

    CurrencyCode enum extended

---
+++ src/PtNet.Entities.Tests/MoneyGeneralTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PtNet.Entities.Tests
+{
+    [TestClass]
+    public class MoneyGeneralTests
+    {
+        [TestMethod]
+        public void Money_default_constructor_should_create_proper_default_Money()
+        {
+            var expected = new Money(0.0m, CurrencyCode.XXX);
+
+            var actual = new Money();
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void ToString_should_return_properly_formatted_string()
+        {
+            var money = new Money(10, CurrencyCode.XXX);
+
+            var expected = "10 XXX";
+
+            var actual = money.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- src/PtNet.Entities/Money.cs
+++ src/PtNet.Entities/Money.cs
@@ -1,97 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PtNet.Entities
 {
     public class Money
     {
         public decimal Amount { get; set; }
-        public CurrencyCode Currency { get; set; }
+        public CurrencyCode Currency { get; }
 
         public Money()
         {
-            new Money(0, CurrencyCode.XXX);
+            new Money(0.0m, CurrencyCode.XXX);
         }
 
         public Money (decimal amount, CurrencyCode currency)
         {
             Amount = amount;
             Currency = currency;
-        }
-
-        public override string ToString()
-        {
-            return $"{Amount} {Currency}";
-        }
-
-        public override bool Equals (object obj)
-        {
-            var item = obj as Money;
-
-            if (item == null)
-            {
-                return false;
-            }
-
-            return (this.Amount == item.Amount) && (this.Currency == item.Currency);
-        }
-        
-        public override int GetHashCode()
-        {
-            return this.GetHashCode();
-        }
-
-        public static Money operator +(Money x, Money y)
-        {
-            if (x.Currency != y.Currency)
-            {
-                throw new InvalidOperationException();
-            }
-
-            return new Money(x.Amount+y.Amount, x.Currency);
-        }
-
-        public static Money operator -(Money x, Money y)
-        {
-            if (x.Currency != y.Currency)
-            {
-                throw new InvalidOperationException();
-            }
-            
-            return new Money(x.Amount-y.Amount, x.Currency);
-        }
-
-        public static Money operator *(Money x, Money y)
-        {
-            if (x.Currency != y.Currency)
-            {
-                throw new InvalidOperationException();
-            }
-            
-            return new Money(x.Amount*y.Amount, x.Currency);
-        }
-
-        public static Money operator /(Money x, Money y)
-        {
-            if (x.Currency != y.Currency)
-            {
-                throw new InvalidOperationException();
-            }
-            
-            return new Money(x.Amount/y.Amount, x.Currency);
-        }
-
-        public static bool operator ==(Money x, Money y)
-        {
-            return (x.Amount == y.Amount) && (x.Currency == y.Currency);
-        }
-
-        public static bool operator !=(Money x, Money y)
-        {
-            return (x.Amount != y.Amount) && (x.Currency != y.Currency);
         }
     }
 }
